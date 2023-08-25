63a2d4106553e1e28e82d6485254124935dac93f
grbalmeida <g.r.almeida@live.com>
2023-08-25 17:40:35 -0300

    Jantar de hoje

---
+++ jantar-de-hoje.js
@@ -0,0 +1,16 @@
+const jantarDeHoje = ['🍔', '🌭', '🍕']
+
+jantarDeHoje.pop(); 
+console.log(jantarDeHoje); // [ '🍔', '🌭' ]
+
+jantarDeHoje.pop();
+console.log(jantarDeHoje); // [ '🍔' ]
+
+jantarDeHoje.push('🍳');
+console.log(jantarDeHoje); // [ '🍔', '🍳' ]
+
+jantarDeHoje.push('🥗');
+console.log(jantarDeHoje); // [ '🍔', '🍳', '🥗' ]
+
+jantarDeHoje.push('🍏');
+console.log(jantarDeHoje); // [ '🍔', '🍳', '🥗', '🍏' ]
