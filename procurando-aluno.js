4d80260d17d902f92a8d59a1b4eb4f203df6e062
grbalmeida <g.r.almeida@live.com>
2023-08-25 19:58:03 -0300

    Desestruturando uma lista

--- procurando-aluno.js
+++ procurando-aluno.js
@@ -4,10 +4,11 @@ const medias = [10, 8, 7.5, 9];
 const listaDeAlunosEMedias = [alunos, medias];
 
 function exibeNomeENota(aluno) {
-    if (listaDeAlunosEMedias[0].includes(aluno)) {
-        const indice = listaDeAlunosEMedias[0].indexOf(aluno);
+    const [alunos, medias] = listaDeAlunosEMedias;
 
-        const mediaDoAluno = listaDeAlunosEMedias[1][indice];
+    if (alunos.includes(aluno)) {
+        const indice = alunos.indexOf(aluno);
+        const mediaDoAluno = medias[indice];
 
         console.log(`${aluno} tem a média ${mediaDoAluno}`);
     } else {
