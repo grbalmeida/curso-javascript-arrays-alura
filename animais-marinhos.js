dd399b5b339839cf0fedf5a83e4f43b62c62444d
grbalmeida <g.r.almeida@live.com>
2023-08-25 19:34:42 -0300

    Animais marinhos

---
+++ animais-marinhos.js
@@ -0,0 +1,7 @@
+const animaisDoAquario = ['🐋', '🐙', '🐬', '🦈'];
+
+animaisDoAquario.splice(1, 0, '🐠');
+console.log(animaisDoAquario); // ['🐋', '🐠', '🐙', '🐬', '🦈']
+
+animaisDoAquario.splice(3, 2, '🐟');
+console.log(animaisDoAquario); // ['🐋', '🐠', '🐙', '🐟']
