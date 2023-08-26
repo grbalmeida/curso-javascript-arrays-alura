e59326d352e183583fc6c649e0d8efa5b0204a23
grbalmeida <g.r.almeida@live.com>
2023-08-25 21:44:33 -0300

    Alterando strings com map()

---
+++ map-nomes.js
@@ -0,0 +1,5 @@
+const nomes = ['ana julia', 'Caio vinicius', 'BIA silva'];
+
+const nomesPadronizados = nomes.map((nome) => nome.toUpperCase());
+
+console.log(nomesPadronizados); // [ 'ANA JULIA', 'CAIO VINICIUS', 'BIA SILVA' ]
