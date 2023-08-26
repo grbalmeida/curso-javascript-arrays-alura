06b19482eb4f04108e08d9ecd5860012b89535b3
grbalmeida <g.r.almeida@live.com>
2023-08-25 22:09:10 -0300

    Removendo elementos repetidos

---
+++ Set.js
@@ -0,0 +1,7 @@
+const nomes = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"];
+
+const meuSet = new Set(nomes);
+
+const nomesAtualizados = [...meuSet];
+
+console.log(nomesAtualizados); // [ 'Ana', 'Clara', 'Maria', 'João' ]
