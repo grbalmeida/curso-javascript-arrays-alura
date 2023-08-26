ada18830bef7e2fbbf486f73e7862c658eaaa726
grbalmeida <g.r.almeida@live.com>
2023-08-25 22:05:10 -0300

    Clonando com spread operator

---
+++ copiando-array.js
@@ -0,0 +1,6 @@
+const notas = [7, 7, 8, 9];
+
+const novasNotas = [...notas, 10];
+
+console.log(`As novas notas são ${novasNotas}`); // As novas notas são 7,7,8,9,10
+console.log(`As notas originais são ${notas}`); // As notas originais são 7,7,8,9
